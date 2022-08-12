00b135c4bfef896a2adff133b03700b6ad36368b
Dmitry Kisler <dmitry.kisler@olx.com>
2022-08-12 16:13:47 +0200

    Add server for local dev

--- Makefile
+++ Makefile
@@ -8,9 +8,7 @@ unittest: ## Runs unit tests.
 	@ go tool cover -func .coverage_temp
 
 WASM_PORT := 9090
-WASM_DIR := ./client/assets
-WASMNAME_BIN := $(WASM_DIR)/logic.wasm
-#GOROOT := `echo GOROOT`
+WASMNAME_BIN := ./client/assets/logic.wasm
 CODE_DIR := ./cmd/wasm
 
 web.setup: ## Sets WASM Go dependencies.
@@ -21,4 +19,4 @@ web.build: ## Compiles the app.
 		go build -o $(WASMNAME_BIN) cmd/wasm/*.go
 
 web.server: ## Run a temp web server.
-	@ PORT=$(WASM_PORT) DIR_WEB=$(WASM_DIR) go run cmd/server/main.go
+	@ PORT=$(WASM_PORT) DIR_WEB=./client/ go run cmd/server/main.go
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,37 @@
+//go:build !unittest
+// +build !unittest
+
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
+var dir, port string
+
+func init() {
+	dir = os.Getenv("DIR_WEB")
+	if dir == "" {
+		log.Fatalln("'DIR_WEB' with web assets to be server must be set as envvar")
+	}
+
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = "9090"
+	}
+}
+
+func handler() http.Handler {
+	log.Printf("open for dev server: http://localhost:%s\n", port)
+	log.Println(dir)
+	return http.FileServer(http.Dir(dir))
+}
+
+func main() {
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), handler()); err != nil {
+		log.Fatalln("Failed to start server", err)
+	}
 }
