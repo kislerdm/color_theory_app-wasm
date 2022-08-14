226e63ec4fa41b039c95b3400eade8272c5d459b
Dmitry Kisler <dmitry.kisler@olx.com>
2022-08-14 13:47:31 +0200

    Improves unit tests coverage.

--- cmd/wasm/logic_test.go
+++ cmd/wasm/logic_test.go
@@ -1,69 +1,98 @@
-//go:build js
-
-// +build,js,!unittest
-
 package main
 
 import (
 	"reflect"
-	"syscall/js"
 	"testing"
 )
 
-var probeHTML string
-
-type procMimic struct{}
-
-func (m *procMimic) GenerateOutput(r, g, b float64) (main.Output, error) {
-	return (&main.p{}).GenerateOutput(r, g, b)
-}
-
-func (m *procMimic) GenerateUI(o main.Output) string {
-	return (&main.p{}).GenerateUI(o)
-}
-
-func (m *procMimic) SetUI(html string) {
-	probeHTML = html
-}
-
-func Test_exec(t *testing.T) {
-	type args struct {
-		p main.processor
-	}
-	type params struct {
-		this js.Value
-		args []js.Value
+func TestOutput_generateUI(t *testing.T) {
+	type fields struct {
+		Name   string
+		IsWarm bool
 	}
 	tests := []struct {
 		name     string
-		args     args
-		params   params
-		wantHTML string
-		want     interface{}
+		fields   fields
+		wantHtml string
 	}{
 		{
-			name: "happy path",
-			params: params{
-				js.Null(), []js.Value{
-					js.ValueOf(93),
-					js.ValueOf(150),
-					js.ValueOf(81),
-				},
+			name: "happy path: cold",
+			fields: fields{
+				Name:   "foo",
+				IsWarm: false,
 			},
-			args:     args{&procMimic{html: ""}},
-			wantHTML: "",
-			want:     nil,
+			wantHtml: `<div><label for="output_name" id="output_label">Color Name:</label><output name="color_name" id="output_name"> foo</output></div><div><label for="output_type" id="output_label">Color Type:</label><output name="color_type" id="output_type"> Cool</output></div>`,
+		},
+		{
+			name: "happy path: warm",
+			fields: fields{
+				Name:   "bar",
+				IsWarm: true,
+			},
+			wantHtml: `<div><label for="output_name" id="output_label">Color Name:</label><output name="color_name" id="output_name"> bar</output></div><div><label for="output_type" id="output_label">Color Type:</label><output name="color_type" id="output_type"> Warm</output></div>`,
+		},
+		{
+			name: "unhappy path: name not found",
+			fields: fields{
+				Name:   "",
+				IsWarm: true,
+			},
+			wantHtml: `<div><label for="output_name" id="output_label">Color Name:</label><output name="color_name" id="output_name"> Not found</output></div><div><label for="output_type" id="output_label">Color Type:</label><output name="color_type" id="output_type"> Warm</output></div>`,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				if got := exec(tt.args.p); !reflect.DeepEqual(got, tt.want) {
-					t.Errorf("exec() = %v, want %v", got, tt.want)
+				o := &Output{
+					Name:   tt.fields.Name,
+					IsWarm: tt.fields.IsWarm,
 				}
+				if gotHtml := o.generateUI(); gotHtml != tt.wantHtml {
+					t.Errorf("generateUI() = %v, want %v", gotHtml, tt.wantHtml)
+				}
+			},
+		)
+	}
+}
 
-				if tt.wantHTML != probeHTML {
-					t.Errorf("wrong generated html exec() = %v, want %v", tt.wantHTML, probeHTML)
+func Test_generateOutput(t *testing.T) {
+	type args struct {
+		r float64
+		g float64
+		b float64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Output
+		wantErr bool
+	}{
+		{
+			name: "happy path",
+			args: args{187, 58, 58},
+			want: Output{
+				Name:   "Well Read",
+				IsWarm: true,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "unhappy path: wrong input",
+			args:    args{-1, 58, 58},
+			want:    Output{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				got, err := generateOutput(tt.args.r, tt.args.g, tt.args.b)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("generateOutput() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("generateOutput() got = %v, want %v", got, tt.want)
 				}
 			},
 		)
--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,10 +1,36 @@
 //go:build js
-
-// +build,js,!unittest
+// +build js
 
 package main
 
+import (
+	"syscall/js"
+)
+
+func sendErr(msg string) map[string]interface{} {
+	return map[string]interface{}{"error": msg}
+}
+
+func run(this js.Value, args []js.Value) interface{} {
+	if len(args) < 3 {
+		return sendErr("no r, g, b input provided")
+	}
+
+	r := args[0].Float()
+	g := args[1].Float()
+	b := args[2].Float()
+
+	o, err := generateOutput(r, g, b)
+	if err != nil {
+		return sendErr(err.Error())
+	}
+
+	js.Global().Get("document").Call("getElementById", "color_output").Set("innerHTML", o.generateUI())
+
+	return nil
+}
+
 func main() {
-	Run()
+	js.Global().Set("start", js.FuncOf(run))
 	select {}
 }
